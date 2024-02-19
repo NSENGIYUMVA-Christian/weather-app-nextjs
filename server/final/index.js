decd1f2271bf304051671b3a0049cec4ec9561dc
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2024-02-19 10:37:07 +0200

    created basic routes

--- server/final/index.js
+++ server/final/index.js
@@ -3,10 +3,22 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 //port
-const port = 8080;
+const port = process.env.PORT || 8080;
 /// middle ware to allow cors
 app.use(cors());
 app.get("/", ({ req, res }) => {
     res.json({ success: true, msg: "testing whether" });
+});
+// login
+app.get("/users/login", ({ req, res }) => {
+    res.json({ success: true, msg: "login" });
+});
+// register
+app.get("/users/register", ({ req, res }) => {
+    res.json({ success: true, msg: "register" });
+});
+// dashboard
+app.get("/users/dashboard", ({ req, res }) => {
+    res.json({ success: true, msg: "dashboard" });
 });
 app.listen(port, () => console.log(`the server is listening to port ${port}...`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,13 +9,25 @@ type resAndReqType = {
 }
 
 //port
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 /// middle ware to allow cors
 app.use(cors());
 
 app.get("/", ({req, res}:resAndReqType) => {
   res.json({ success: true, msg: "testing whether" });
+});
+// login
+app.get("/users/login", ({req, res}:resAndReqType) => {
+  res.json({ success: true, msg: "login" });
+});
+// register
+app.get("/users/register", ({req, res}:resAndReqType) => {
+  res.json({ success: true, msg: "register" });
+});
+// dashboard
+app.get("/users/dashboard", ({req, res}:resAndReqType) => {
+  res.json({ success: true, msg: "dashboard" });
 });
 
 app.listen(port, () => console.log(`the server is listening to port ${port}...`));
