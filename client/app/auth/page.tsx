bffe6a9a95e034794a28e7407ff9871169fc68b0
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2024-02-20 12:42:02 +0200

    basic login success

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import axios from "axios"
 
-const authPage = () => {
+const AuthPage = () => {
+  const [formData, setFormData] = useState<any>({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  /// submit form data
+  const submitFormData = async(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    try {
+        const {data} = await axios.post("http://localhost:8080/api/v1/auth/login",formData)
+       // console.log('form data is', formData);
+        console.log("data is",data)
+    } catch (error) {
+        console.log("there was an error")
+    }
+
+   
+  };
+
   return (
-    <div>authPage</div>
-  )
-}
+    <div>
+      <h2>Form</h2>
+      <form >
+        <input type='text' name='username' value={formData.username} onChange={handleChange} />
+        <input type='password' name='password' value={formData.password} onChange={handleChange} />
+        <button type='submit' onClick={submitFormData}>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default authPage
+export default AuthPage;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,23 +5,7 @@ import { useEffect } from "react";
 import { useAppContext } from "@/context";
 
 
-
-
-// const getData = async()=>{
-//  // await new Promise((resolve)=>setTimeout(resolve,1000))
-//    const response = await fetch("http://localhost:8080/");
-//    /// throw error
-//    if(!response.ok )
-//    {
-//      throw new Error('Failed to fetch drinks')
-//    }
-//    const data = await response.json();
-//    return data
-//  }
-
 export default function Home() {
-  // const data = await getData()
-  // console.log("ok here", data)
 
   const {name,setName} = useAppContext()
 
