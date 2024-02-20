9043d925699930db65bf76685af390d3ba78801b
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2024-02-20 13:19:16 +0200

    added toggle from login to register

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 import axios from "axios"
 
 const AuthPage = () => {
+    /// user data
   const [formData, setFormData] = useState<any>({
     username: '',
     password: ''
   });
+  /// is user has an account
+  const [hasAccountAlready,setHasAccountAlready] = useState<boolean>(false)
 
+///// handle form change
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
@@ -22,18 +26,28 @@ const AuthPage = () => {
     } catch (error) {
         console.log("there was an error")
     }
-
-   
   };
 
   return (
     <div>
       <h2>Form</h2>
-      <form >
-        <input type='text' name='username' value={formData.username} onChange={handleChange} />
-        <input type='password' name='password' value={formData.password} onChange={handleChange} />
-        <button type='submit' onClick={submitFormData}>Submit</button>
-      </form>
+   {hasAccountAlready ?
+   <form > 
+   <h2>Login</h2>
+   <input type='text' name='username' value={formData.username} onChange={handleChange} />
+   <input type='password' name='password' value={formData.password} onChange={handleChange} />
+   <button type='submit' onClick={submitFormData}>Submit</button>
+  <p>Don't have an account? <span> <button type='button' onClick={()=>setHasAccountAlready(false)} >Register</button> </span></p> 
+ </form> : 
+  <form >
+  <h2>Register</h2>
+  <input type='text' name='username' value={formData.username} onChange={handleChange} />
+  <input type='password' name='password' value={formData.password} onChange={handleChange} />
+  <button type='submit' onClick={submitFormData}>Submit</button>
+ <p>Have an account? <span> <button type='button' onClick={()=>setHasAccountAlready(true)} >Login</button> </span></p> 
+</form>
+}
+      
     </div>
   );
 };
